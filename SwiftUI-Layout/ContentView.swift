fbf6b8f03c809bce217925ee68095edac2c6071f
Botao Li <1114338553@qq.com>
2019-07-05 22:35:38 +0800

    layout
    Some basic layout and ui

--- SwiftUI-Layout/ContentView.swift
+++ SwiftUI-Layout/ContentView.swift
@@ -12,40 +12,28 @@ struct ContentView : View {
 	var body: some View {
 		ZStack {
 			
-			VStack {
-				Text("123")
-			}
-			.frame(width: 300.0, height: 220.0)
-				.background(Color.blue)
-				.cornerRadius(10)
-				.shadow(radius: 20)
-				.offset(x: 0, y: -20)
-
+			TitleView()
+				.blur(radius: 20)
 			
-			VStack {
-				HStack {
-					VStack(alignment: .leading) {
-						Text("UI Design")
-							.font(.headline)
-							.fontWeight(.bold)
-							.color(Color("accent"))
-							.padding(.top)
-						Text("Certificate")
-							.color(.white)
-					}
-					Spacer()
-					Image("Logo")
-						.resizable()
-						.frame(width: 30.0, height: 30.0)
-				}
-				.padding(.horizontal)
-				Spacer()
-				Image("Background")
-			}
-			.frame(width: 340.0, height: 220.0)
-				.background(Color.black)
-				.cornerRadius(10)
-				.shadow(radius: 20)
+			CardBottomView()
+				.blur(radius: 20)
+			
+			CardView()
+				.offset(x: 0, y: -40)
+				.scaleEffect(0.85)
+				.rotationEffect(Angle(degrees: 15))
+				.rotation3DEffect(Angle(degrees: 50), axis: (x: 10.0, y: 10.0, z: 10.0))
+			
+			CardView()
+				.offset(x: 0, y: -20)
+				.scaleEffect(0.9)
+				.rotationEffect(Angle(degrees: 10))
+				.blendMode(.hardLight)
+			
+			CertificateView()
+				.scaleEffect(0.95)
+				.rotationEffect(Angle(degrees: 5))
+			
 		}
 	}
 }
@@ -57,3 +45,82 @@ struct ContentView_Previews : PreviewProvider {
 	}
 }
 #endif
+
+struct CardView : View {
+	var body: some View {
+		return VStack {
+			Text("123")
+		}
+		.frame(width: 340.0, height: 220.0)
+			.background(Color.blue)
+			.cornerRadius(10)
+			.shadow(radius: 20)
+	}
+}
+
+struct CertificateView : View {
+	var body: some View {
+		return VStack {
+			HStack {
+				VStack(alignment: .leading) {
+					Text("UI Design")
+						.font(.headline)
+						.fontWeight(.bold)
+						.color(Color("accent"))
+						.padding(.top)
+					Text("Certificate")
+						.color(.white)
+				}
+				Spacer()
+				Image("Logo")
+					.resizable()
+					.frame(width: 30.0, height: 30.0)
+			}
+			.padding(.horizontal)
+			Spacer()
+			Image("Background")
+		}
+		.frame(width: 340.0, height: 220.0)
+			.background(Color.black)
+			.cornerRadius(10)
+			.shadow(radius: 20)
+	}
+}
+
+struct TitleView : View {
+    var body: some View {
+        return VStack {
+            HStack {
+                Text("Certificates")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    Spacer()
+                }
+                Image("Illustration5")
+                Spacer()
+                }.padding()
+            }
+        }
+
+        struct CardBottomView : View {
+            var body: some View {
+                return VStack(spacing: 20.0) {
+                    Rectangle()
+                        .frame(width: 60, height: 6)
+                        .cornerRadius(3.0)
+                        .opacity(0.1)
+					
+					
+                        Text("This certificate is proof that Tony This certificate is proof that Tony This certificate is proof that Tony")
+                            .lineLimit(10)
+                            Spacer()
+                        }
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .padding()
+                        .padding(.horizontal)
+                        .background(Color.white)
+                        .cornerRadius(30)
+                        .shadow(radius: 20)
+                        .offset(y: 600)
+                }
+            }
